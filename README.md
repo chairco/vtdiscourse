f0603b866fdb329151f7d328ba99f1b09c3676e3
chairco <chairco@gmail.com>
2016-12-26 13:07:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vtdiscourse
-this is g0v vTaiwan project
+This is g0v vTaiwan project 用來處理自動建立類別工具
---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+    vtdiscourse
+    ~~~~
+
+    This is g0v vTaiwan Project, it's can help develope easy create topic on talk vTaiwan
+    web site from gitbook
+
+    :copyright: (c) 2016 by chairco <chairco@gmail.com>.
+    :license: BSD.
+"""
+
+import uuid
+
+from pip.req import parse_requirements  
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import vtdiscourse
+
+
+def requirements(path):  
+    return [str(r.req) for r in parse_requirements(path, session=uuid.uuid1())]
+
+
+class Tox(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['-v', '-epy']
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        tox.cmdline(self.test_args)
+
+
+setup(  
+    name='vtdiscourse',
+    version=vtdiscourse.__version__,
+    author=vtdiscourse.__author__,
+    author_email=vtdiscourse.__email__,
+    url='http://your.host.name/demo',
+    description='Help to create topic on talk.vTaiwan web.',
+    long_description=__doc__,
+    packages=find_packages(),
+    install_requires=requirements('requirements.txt'),
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
